38bfc27f94d0f68378aa99e3919d10e32105b860
Siva Nagi Reddy Alla <siva.alla@outlook.com>
2022-03-16 22:23:51 +0530

    Learn basics and props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header(props) {
+  console.log(props);
+  return(
+      <header>
+        <h1>{props.name} IT Company</h1>
+      </header>
+  );
+}
+
+function Main(props) {
+  return(
+      <section>
+        <p>We create {props.adjective} apps</p>
+        <ul style={{textAlign: "left"}}>
+          {props.technologies.map((tech) => <li key={tech.id}>{tech.title}</li>)}
+        </ul>
+      </section>
+  );
+}
+
+function Footer(props) {
+  return(
+      <footer>
+        <h4>{props.year} copyright</h4>
+      </footer>
+  );
+}
+
+const technologies = [
+    "React",
+    "Java",
+    "Go",
+    "Python",
+    "JavaScript"
+];
+
+const techObjects = technologies.map((tech, i) => ({id: i, title: tech}));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Siva's"/>
+      <Main adjective="best" technologies={techObjects}/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
