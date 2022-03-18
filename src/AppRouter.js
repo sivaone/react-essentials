9e9033f4a04d42077a248d6f63fed8ac38ab9562
Siva Nagi Reddy Alla <siva.alla@outlook.com>
2022-03-18 12:34:54 +0530

    Add nested routes

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,16 +1,29 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { Home, About, Events, Contact, Err404 } from "./pages";
+import {Routes, Route} from "react-router-dom";
+import {
+    Home,
+    About,
+    Events,
+    Contact,
+    Err404,
+    Services,
+    CompanyHistory,
+    Location
+} from "./pages";
 
 function AppRouter() {
-    return(
+    return (
         <div>
             <Routes>
-                <Route path={"/"} element={<Home />}/>
-                <Route path={"/about"} element={<About />}/>
-                <Route path={"/events"} element={<Events />}/>
-                <Route path={"/contact"} element={<Contact />}/>
-                <Route path={"*"} element={<Err404 />}/>
+                <Route path={"/"} element={<Home/>}/>
+                <Route path={"/about"} element={<About/>}>
+                    <Route path={"services"} element={<Services />}/>
+                    <Route path={"history"} element={<CompanyHistory/>}/>
+                    <Route path={"location"} element={<Location/>}/>
+                </Route>
+                <Route path={"/events"} element={<Events/>}/>
+                <Route path={"/contact"} element={<Contact/>}/>
+                <Route path={"*"} element={<Err404/>}/>
             </Routes>
         </div>
     )
